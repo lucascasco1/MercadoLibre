d645b04b2a89a4db0766b443e5d31d1e2d429f07
Lucas Casco <thundestruck.lc@gmai.com>
2021-02-09 16:14:47 -0300

    funcionando

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -3,11 +3,10 @@ const fetch = require('node-fetch')
 
 
 server.post('/', (req, res) => {
-	// const q= req.query.q; 
-    // let offset = 0;
+
 	const {q,offset} = req.body;
 	console.log(q, "esto es q")
-	console.log (offset, "offset")
+	console.log (offset, "esto es offset")
 	fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${q}&offset=${offset}&limit=5`).then (data=>data.json()).then (response=>res.send(response))
 });
 
